6f42a053f592626c46a904a94571afd2fab373e4
Nicholas Kastanos <nicholaskastanos@gmail.com>
2020-06-03 21:20:15 +0200

    Adds new alchemy route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'alchemy',
+    loadChildren: () => import('./alchemy/alchemy.module').then( m => m.AlchemyModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent implements OnInit {
   public selectedIndex = 0;
-  public appPages = [
+  public alchemyPages = [
     {
       title: 'Inbox',
       url: '/folder/Inbox',
@@ -63,7 +63,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const path = window.location.pathname.split('folder/')[1];
     if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+      this.selectedIndex = this.alchemyPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
     }
   }
 }
