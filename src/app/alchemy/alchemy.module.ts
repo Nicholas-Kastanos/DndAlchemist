1bfc97c16c4f3338481804459bb6179556fa31c5
Nicholas Kastanos <nicholaskastanos@gmail.com>
2020-06-04 16:57:57 +0200

    Fix alchemy routing module

--- src/app/alchemy/alchemy.module.ts
+++ src/app/alchemy/alchemy.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { AlchemyRoutingModule } from './alchemy-routing.module';
 
+
 @NgModule({
+  declarations: [],
   imports: [
     CommonModule,
-    FormsModule,
-    IonicModule
-  ],
-  declarations: []
+    AlchemyRoutingModule
+  ]
 })
-export class AlchemyModule {}
+export class AlchemyModule { }
