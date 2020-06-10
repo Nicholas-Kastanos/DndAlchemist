5a0019509b2a269f72518dc0ae03f95cce417557
Anita <demellokochanita@gmail.com>
2020-06-10 20:26:52 +0200

    forage form (this form sucks and I hate it)

--- src/app/alchemy/forage/forage.page.ts
+++ src/app/alchemy/forage/forage.page.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-forage',
@@ -9,19 +9,68 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class ForagePage implements OnInit {
     private forageInput: FormGroup;
+    indeterminateState: boolean;
+    checkParent: boolean;
+    biomes: any;
+
+    biomes = [
+        
+    ]
 
     constructor(private formBuilder: FormBuilder) {
+        this.forageInput = new FormGroup({
+          forageCheck: new FormControl(''),
+          biomeList: new FormControl('')
+        })
+
         this.forageInput = this.formBuilder.group({
-            forageCheck: ['']
+            forageCheck: ['value'],
+            biomeList: this.formBuilder.array([], [Validators.required])
         });
+        this.biomes = [
+            {value:"Arctic", checked: false}, 
+            {value:"Coast", checked: false},
+            {value:"Desert", checked: false},
+            {value:"Forest", checked: false},
+            {value:"Grassland", checked:false},
+            {value:"Hill", checked: false},
+            {value:"Mountain", checked: false},
+            {value:"Swamp", checked: false},
+            {value:"Underdark", checked: false}
+        ]
     }
     
     ngOnInit() {}
 
-    
+    updateCheckControl(cal, o){
+      if (o.checked){
+        cal.push(new FormControl(o.value));
+      }else{
+        cal.controls.forEach((item: FormControl, index) => {
+          if (item.value == o.value){
+            cal.removeAt(index);
+            return;
+          }
+        })
+      }
+    }
+
+    onLoadCheckboxStatus() {
+      const biomeList: FormArray = this.forageInput.get('biomeList') as FormArray;
+      this.biomes.forEach(o => {
+        this.updateCheckControl(biomeList, o);
+      })
+    }
+
+    onSelectionChange(e, i){
+      const biomeList: FormArray = this.forageInput.get('biomeList') as FormArray;
+      this.biomes[i].checked = e.target.checked;
+      this.updateCheckControl(biomeList, e.target);
+    }
 
     submit(){
         console.log("form submitted");
-    }
+        console.log(this.forageInput.value);
+    } 
 
 }
