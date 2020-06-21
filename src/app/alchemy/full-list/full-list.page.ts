5aee7216178dfae94dfd8ce0aeccb3b4581dfd15
Anita <demellokochanita@gmail.com>
2020-06-21 12:23:31 +0200

    List is collapsible

--- src/app/alchemy/full-list/full-list.page.ts
+++ src/app/alchemy/full-list/full-list.page.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {DatabaseService} from 'src/app/shared/services/database.service';
-import {BaseConcoction} from 'src/app/shared/classes/base-concoction/base-concoction';
-import {Ingredient} from 'src/app/shared/classes/ingredient/ingredient';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from 'src/app/shared/services/database.service';
+import { BaseConcoction } from 'src/app/shared/classes/base-concoction/base-concoction';
+import { Ingredient } from 'src/app/shared/classes/ingredient/ingredient';
 
 @Component({
     selector: 'app-full-list',
@@ -13,25 +13,36 @@ export class FullListPage implements OnInit {
     baseConcoctions: BaseConcoction[];
     ingredients: Ingredient[];
 
-    baseConcoctionsDisplay: BaseConcoction[];
-    ingredientsDisplay: Ingredient[];
+    baseConcoctionsDisplay: BaseConcoction[] = [];
+    ingredientsDisplay: Ingredient[] = [];
 
     constructor(
         private database: DatabaseService
-    ) {}
+    ) { }
 
-    ngOnInit() {}
-
-    loadData() {
-        this.database.getBaseConcoctions()
-        .then((result) => {
-            this.baseConcoctions = result;
-            this.baseConcoctionsDisplay = result;
-        })
-        this.database.getIngredients()
-        .then((result) => {
-            this.ingredients = result;
-            this.ingredientsDisplay = result;
+    ngOnInit() {
+        this.database.initialiseSubject.subscribe(() => {
+            this.database.getBaseConcoctions()
+                .then((result) => {
+                    this.baseConcoctions = result;
+                })
+            this.database.getIngredients()
+                .then((result) => {
+                    this.ingredients = result;
+                })
         })
     }
+
+    toggleBase(){
+        this.baseConcoctionsDisplay = this.baseConcoctionsDisplay.length === 0 ?
+            this.baseConcoctions :
+            [];
+    }
+
+    toggleIngredients(){
+        this.ingredientsDisplay = this.ingredientsDisplay.length === 0 ?
+            this.ingredients :
+            [];
+    }
+
 }
