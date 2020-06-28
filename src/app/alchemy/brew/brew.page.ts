a465f64900410deb269c2e31925f2e44cee18d77
Anita <demellokochanita@gmail.com>
2020-06-28 19:00:31 +0200

    some changes

--- src/app/alchemy/brew/brew.page.ts
+++ src/app/alchemy/brew/brew.page.ts
@@ -4,6 +4,8 @@ import {Ingredient} from '../../shared/classes/ingredient/ingredient';
 import {BaseConcoction} from '../../shared/classes/base-concoction/base-concoction';
 import {Concoction} from '../../shared/classes/concoction/concoction';
 import {DatabaseService} from '../../shared/services/database.service';
+import {Essence} from '../../shared/classes/essence/essence';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
     selector: 'app-brew',
@@ -16,6 +18,8 @@ export class BrewComponent implements OnInit {
     ingredients: Ingredient[];
 
     newConcoction: Concoction;
+
+    requiredEssences: any[];
 
     constructor(
         public modalCtrl: ModalController,
@@ -31,10 +35,64 @@ export class BrewComponent implements OnInit {
                 .then((result) => {
                     this.ingredients = result;
                 })
+                this.getRequired();
         })
 
         this.newConcoction = this.concoction;
-        console.log(this.concoction)
-        console.log(this.newConcoction)
+    }
+
+    getRequired(){
+        console.debug('dhdhdhdhdhd')
+        console.debug(JSON.stringify(this.baseConcoction.baseEssences))
+
+        for(var x = 0; x < this.baseConcoction.baseEssences.length; x++){
+            this.requiredEssences.push({essence: this.baseConcoction.baseEssences[x], fulfilled: false});
+        }
+
+        for(var x = 0; x < this.concoction.essences.length; x++){
+            this.requiredEssences.push({essence: this.concoction.essences[x], fulfilled: false});
+        }
+        console.debug(JSON.stringify(this.requiredEssences))
+        this.baseConcoction.baseEssences.forEach(essence => {
+            var item = {essence: essence, fulfilled: false}
+            this.requiredEssences.push(item)
+            console.debug('ananananananana')
+        })
+        this.concoction.essences.forEach(essence => {
+            var item = {essence: essence, fulfilled: false}
+            this.requiredEssences.push(item)
+        })
+    }
+
+    select(item: any){
+        console.debug(JSON.stringify(item))
+    }
+
+    getElement(elementId: number){
+        // 1: air
+        // 2 earth
+        // 3 ether
+        // 4 fire
+        // 5 water
+        switch(elementId){
+            case 1: {
+                return "../../assets/img/air.png";
+            }
+            case 2: {
+                return "../../assets/img/earth.png";
+            }
+            case 3: {
+                return "../../assets/img/ether.png";
+            }
+            case 4: {
+                return "../../assets/img/fire.png";
+            }
+            case 5: {
+                return "../../assets/img/water.png";
+            }
+            default: {
+                return "../../assets/img/new_moon.svg";
+            }
+        }
     }
 }
