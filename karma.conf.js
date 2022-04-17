f2ce78ec6fe8d3485b2c15d059cc25155477a2f0
Nicholas Kastanos <nicholaskastanos@gmail.com>
2022-04-17 10:01:51 +0200

    Try to fix TypeORM

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const { IonicModule } = require('@ionic/angular');
+
 module.exports = function (config) {
-  config.set({
+  IonicModule.forRoot({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
--- src/app/shared/services/orm.service.ts
+++ src/app/shared/services/orm.service.ts
@@ -1,53 +1,41 @@
 import { Injectable } from '@angular/core';
 import "reflect-metadata";
 import { Platform } from '@ionic/angular';
-import { createConnection, Connection, ConnectionOptions } from 'typeorm';
+import { DataSource } from "typeorm"
 import { ReplaySubject } from 'rxjs';
-import { CordovaConnectionOptions } from 'typeorm/driver/cordova/CordovaConnectionOptions';
-import { SqljsConnectionOptions } from 'typeorm/driver/sqljs/SqljsConnectionOptions';
+import { CapacitorConnectionOptions } from 'typeorm/driver/capacitor/CapacitorConnectionOptions';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx'
 
 @Injectable({
     providedIn: 'root'
 })
 export class OrmService {
     public initialiseSubject = new ReplaySubject<void>();
-    private connection: Connection;
+    private dataSource: DataSource;
 
-    private connectionOptions: CordovaConnectionOptions;
+    private connectionOptions: CapacitorConnectionOptions;
 
-    constructor(private platform: Platform) {
-        // if (this.platform.is('capacitor') || this.platform.is("cordova")) {
-            this.connectionOptions = {
-                type: "cordova",
-                database: "alchemy2.db",
-                location: "default",
-                synchronize: true,
-                logging: false,
-                entities: [
-                    "src/entity/**/*.ts"
-                ],
-                migrations: [
-                    "src/migration/**/*.ts"
-                ],
-                subscribers: [
-                    "src/subscriber/**/*.ts"
-                ],
-                cli: {
-                    "entitiesDir": "src/entity",
-                    "migrationsDir": "src/migration",
-                    "subscribersDir": "src/subscriber"
-                }
-            };
-        // }
-        // else {
-        //     connectionOptions = {
-        //         ...connectionOptions,
-        //         type: 'sqljs',
-        //         location: 'browser',
-        //         autoSave: true,
-        //         sqlJsConfig: { locateFile: file => `static/js/sql-wasm.wasm` }
-        //     };
-        // }
+    constructor(private platform: Platform, private sqlite: SQLite) {
+        this.connectionOptions = {
+            type: "capacitor",
+            database: "alchemy2.db",
+            synchronize: true,
+            logging: false,
+            entities: [
+                "src/entity/**/*.ts"
+            ],
+            migrations: [
+                "src/migration/**/*.ts"
+            ],
+            subscribers: [
+                "src/subscriber/**/*.ts"
+            ],
+            driver: await this.sqlite.create({
+                name: 'alchemy2.db',
+                location: 'default'
+              })
+        };
+        
         createConnection(this.connectionOptions)
             .then(connection => {
                 this.connection = connection
