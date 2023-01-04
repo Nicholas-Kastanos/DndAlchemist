c4c30a4b1097cc0a3f40be2a31dda28068013c45
Nicholas Kastanos <nicholaskastanos@gmail.com>
2023-01-04 12:33:57 +0200

    Gemtech Slides Update

--- src/app/gemtech/gemtech.page.ts
+++ src/app/gemtech/gemtech.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore from 'swiper';
+import { IonicSlides } from '@ionic/angular';
+
+SwiperCore.use([IonicSlides])
 
 @Component({
   selector: 'app-gemtech',
